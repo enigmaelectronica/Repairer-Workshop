af94a9cf41c57508c12d63d0d86911d0d8ab42e3
Enya Andrea Ribba Hernández <enigmahernandez@gmail.com>
2021-07-30 12:02:45 -0600

    Add files via upload
    El archivo se encuentra en esta ubicación
    
    \application\language\spanish

---
+++ pos_lang.php
@@ -0,0 +1,120 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+* Name:  POS Lang - Español
+*
+* Author: EnigmaTeam
+* 		  webmaster@enigmateam.org
+*         @enigmahernandez
+*
+* Location: http://github.com/benedmunds/enigmaelectronica/
+*
+* Created:  30.07.2021
+*/
+
+// POS Lenguaje
+$lang['sales_person']			= "Asociado de ventas";
+$lang['sale_no_ref']			= "Venta No./Ref";
+$lang['customer']				= "Cliente";
+$lang['paid_by']				= "Pagado por";
+$lang['amount']					= "Monto";
+$lang['change']					= "Cambio";
+$lang['cash']					= "Efectivo";
+$lang['no']						= "No";
+$lang['CC']						= "Tarjeta Crédito";
+$lang['cheque']					= "Cheque";
+$lang['cc_no']					= "No. Tarjeta Crédito";
+$lang['cc_holder']				= "Nombre Titular";
+$lang['card_type']				= "Tipo de Tarjeta";
+$lang['Visa']					= "Visa";
+$lang['MasterCard']				= "MasterCard";
+$lang['Amex']					= "Amex";
+$lang['Discover']				= "Discover";
+$lang['month']					= "Mes";
+$lang['year']					= "Año";
+$lang['cvv2']					= "CVV2";
+$lang['cheque_no']				= "Cheque No";
+$lang['Visa']					= "Visa";
+$lang['MasterCard']				= "MasterCard";
+$lang['Amex']					= "Amex";
+$lang['from']					= "De";
+$lang['to']						= "Para";
+$lang['sales']					= "Ventas";
+
+// Informe de Caja - Exportación
+$lang['cc_slips']				= "Recibos de tarjeta de crédito";
+$lang['total_cash']				= "Efectivo Total";
+$lang['open_time']				= "Hora Apertura";
+$lang['close_time']				= "Hora Cierre";
+$lang['opened_by']				= "Abierto por";
+$lang['closed_by']				= "Cerrado por";
+$lang['cash_in_hand']			= "Efectivo en mano";
+$lang['cheques']				= "Cheques";
+$lang['count_note']				= "Nota de Cuenta";
+$lang['cc_slips_submitted']		= "Recibos de tarjeta de crédito enviados";
+$lang['cheques_submitted']		= "Cheques enviados";
+$lang['total_cash_submitted']	= "Total Efectivo enviado";
+$lang['drawer_report']			= "Reporte de Caja";
+$lang['no_record_found']		= "No se encontró registro";
+$lang['drawer_report_Date']		= "Reporte de caja de %s a %s";
+$lang['sale_details']			= "Detalle de venta";
+$lang['view_sale']				= "Ver Venta";
+$lang['date_range']				= "Rango de fecha";
+$lang['sale_id']				= "ID de Venta";
+$lang['close_cash']				= "Cierre de Caja";
+$lang['#']						= "#";
+$lang['back_to_pos']			= "Regresar a POS";
+$lang['opening_Cash']			= "Apertura en Efectivo";
+$lang['cash_sales']				= "Ventas en efectivo";
+$lang['check_sales']			= "Consultar ventas";
+$lang['cc_sales']				= "Ventas Tarjeta de Crédito";
+$lang['pppsales']				= "Ventas PayPal";
+$lang['othersales'] 			= "Otras ventas";
+$lang['totalsales'] 			= "Total Ventas";
+$lang['cash_count_sheet'] 		= "Hoja Conteo Efectivo";
+$lang['open_register'] 			= "Abrir Caja/Turno";
+$lang['total_value_cash'] 		= "Valor total en Efectivo";
+$lang['total_value_check'] 		= "Valor total en Cheques";
+$lang['total_value_ccc'] 		= "Valor total en Tarjetas de Crédito";
+$lang['register_closing_report']		= "Reporte de cierre de Registro";
+$lang['register_closing_report_span']	= "Abierto en %s a %s ||| Cerrado en %s a %s";
+$lang['select_pos_Client'] 		= "Por favor seleccione un cliente o agregue uno nuevo";
+$lang['walk_in'] 				= "Consumidor Final";
+$lang['max_allowed_limit_reached'] 		= "Se alcanzó el límite máximo permitido";
+$lang['stock_error'] 			= "Usted solo tiene";
+$lang['pos_not_in_stock'] 		= "Usted no tiene este producto en stock";
+$lang['paid_error'] 			= 'Parece que todavía se debe algo de dinero. Cobra el saldo restante del cliente.';
+$lang['add_product_before_checkout'] 	= 'Agregue un producto antes del pago.';
+$lang['checkout_negative'] 				= 'El pago no puede ser negativo.';
+$lang['sale_note'] 				= 'Nota de venta';
+$lang['tpayments'] 				= 'Total de pagos';
+$lang['add_more_payments'] 		= 'Agregar más pagos';
+$lang['check_number'] 			= 'Número de Cheque';
+$lang['payment_note'] 			= 'Nota de pago';
+$lang['paying_by'] 				= 'Pagando por';
+$lang['select_payment_method'] 	= 'Seleccione el método de pago';
+$lang['ppp'] 					= 'PayPal';
+$lang['other'] 					= 'Otro';
+$lang['finalize_sale'] 			= 'Finalizar venta';
+$lang['payment'] 				= 'Pago';
+$lang['clear_sale'] 			= 'Limpiar Venta';
+$lang['product'] 				= 'Producto';
+$lang['qty'] 					= 'Cant.';
+$lang['register_closed'] 		= 'Registro cerrado con éxito';
+$lang['register_opened'] 		= 'Registro abierto con éxito';
+$lang['register_open'] 			= 'Registro abierto';
+$lang['register_not_open']		= 'Registro no abierto';
+$lang['register_details'] 		= 'Detalles de registro';
+$lang['close_register']			= 'Cerrar registro';
+$lang['update_settings']		= "Actualizar configuraciones";
+$lang['products_per_page']		= "Mostrar Productos";
+$lang['pos_config']				= "POS Config";
+$lang['pos_setting_updated']	= "La configuración de POS se actualizó correctamente";
+$lang['product_button_color']	= "Color del botón del producto";
+$lang['default']				= "Predeterminado";
+$lang['primary']				= "Primario";
+$lang['info']					= "Info";
+$lang['warning']				= "Advertencia";
+$lang['danger']					= "Cuidado";
+$lang['search_product_by_name_code']	= "Escanear/Buscar producto por nombre/código";
+$lang['au_pr_name_tip']			= "Escriba el código/nombre para obtener sugerencias o simplemente escanee con el código de barras";
+$lang['select_customer']		= "Seleccione Cliente primero";
